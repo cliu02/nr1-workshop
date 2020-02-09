87589100133ecfcd620925ee4709943ee4fac440
Crystal Liu <cliu02@DNa1d6584.SUNet>
2020-02-08 20:52:47 -0800

    Add lab1 setup

---
+++ lab1/nerdlets/lab1-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Lab1NerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, lab1-nerdlet Nerdlet!</h1>;
+    }
+}
